d9c26ff55f1eed7e0de4b11b65eb2a0c4e344263
Daniel KT <dan@distrosync.com>
2020-11-15 19:29:30 -0500

    use lorri with xmonad source to improve editing experience

---
+++ xmonad/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  haskell = pkgs.haskellPackages.ghcWithPackages (p: [
+      p.xmonad
+      p.xmonad-contrib
+      p.xmonad-extras
+      p.haskell-language-server
+    ]);
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    # keep this line if you use bash
+    haskell
+    pkgs.bashInteractive
+  ];
+}
